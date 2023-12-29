278140456e94c2ae57926698cc70273a0974a0f6
Doyle Brown <dalpiad@gmail.com>
2023-12-29 11:17:47 -0600

    Completed task 4.

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,57 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob1, testJob2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob1 = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        assertTrue(testJob1.getName() instanceof String);
+        assertTrue(testJob1.getEmployer() instanceof Employer);
+        assertTrue(testJob1.getLocation() instanceof Location);
+        assertTrue(testJob1.getPositionType() instanceof PositionType);
+        assertTrue(testJob1.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals(testJob1.getName(), "Product tester");
+        assertEquals(testJob1.getEmployer().getValue(), "ACME");
+        assertEquals(testJob1.getLocation().getValue(), "Desert");
+        assertEquals(testJob1.getPositionType().getValue(), "Quality control");
+        assertEquals(testJob1.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob1 = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        Job testJob2 = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+
+        assertEquals(testJob1.equals(testJob2), false);
+    }
 }
